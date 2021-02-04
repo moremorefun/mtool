4dc55d54a725c2bba62f6dfc607b56db81e11807
hao <unnamed.hao@gmail.com>
2021-02-04 18:51:17 +0800

    add kuaidi

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.15
 
 require (
 	github.com/disintegration/imaging v1.6.2
+	github.com/elazarl/goproxy v0.0.0-20210110162100-a92cc753f88e // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/json-iterator/go v1.1.9
 	github.com/onsi/ginkgo v1.15.0 // indirect
 	github.com/onsi/gomega v1.10.5 // indirect
+	github.com/parnurzeal/gorequest v0.2.16
 	github.com/qiniu/go-sdk/v7 v7.9.0
 	github.com/schemalex/schemalex v0.1.1
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	go.uber.org/zap v1.16.0
+	moul.io/http2curl v1.0.0 // indirect
 )
---
+++ mkuaidi100/kuaidi100.go
@@ -0,0 +1,201 @@
+package mkuaidi100
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/parnurzeal/gorequest"
+)
+
+/*
+	0	在途	快件处于运输过程中
+	1	揽收	快件已由快递公司揽收
+	2	疑难	快递100无法解析的状态，或者是需要人工介入的状态， 比方说收件人电话错误。
+	3	签收	正常签收
+	4	退签	货物退回发货人并签收
+	5	派件	货物正在进行派件
+	6	退回	货物正处于返回发货人的途中
+	7	转投	货物转给其他快递公司邮寄
+	10	待清关	货物等待清关
+	11	清关中	货物正在清关流程中
+	12	已清关	货物已完成清关流程
+	13	清关异常	货物在清关过程中出现异常
+	14	拒签	收件人明确拒收
+*/
+const (
+	StateOnTheWay       = 0
+	StateCollect        = 1
+	StateDifficult      = 2
+	StateSignFor        = 3
+	StateReturnSignFor  = 4
+	StateDispatch       = 5
+	StateReturnOnTheWay = 6
+	StateSwitching      = 7
+	StateToBeCleared    = 10
+	StateClearing       = 11
+	StateCleared        = 12
+	StateClearError     = 13
+	StateReject         = 14
+)
+
+// StKuaidi100PollResp 推送
+type StKuaidi100PollResp struct {
+	Result     bool   `json:"result"`
+	ReturnCode string `json:"returnCode"`
+	Message    string `json:"message"`
+}
+
+// StKuaidi100GetResp 获取
+type StKuaidi100GetResp struct {
+	Message string `json:"message"`
+	Nu      string `json:"nu"`
+	Ischeck string `json:"ischeck"`
+	Com     string `json:"com"`
+	Status  string `json:"status"`
+	Data    []struct {
+		Time     string `json:"time"`
+		Context  string `json:"context"`
+		Ftime    string `json:"ftime"`
+		AreaCode string `json:"areaCode"`
+		AreaName string `json:"areaName"`
+		Status   string `json:"status"`
+	} `json:"data"`
+	State     string `json:"state"`
+	Condition string `json:"condition"`
+	RouteInfo struct {
+		From struct {
+			Number string `json:"number"`
+			Name   string `json:"name"`
+		} `json:"from"`
+		Cur struct {
+			Number string `json:"number"`
+			Name   string `json:"name"`
+		} `json:"cur"`
+		To struct {
+			Number string `json:"number"`
+			Name   string `json:"name"`
+		} `json:"to"`
+	} `json:"routeInfo"`
+	IsLoop bool `json:"isLoop"`
+}
+
+// StKuaidi100CbBody 回调
+type StKuaidi100CbBody struct {
+	Message    string `json:"message"`
+	ComOld     string `json:"comOld"`
+	Status     string `json:"status"`
+	LastResult struct {
+		Nu        string `json:"nu"`
+		Message   string `json:"message"`
+		Ischeck   string `json:"ischeck"`
+		Com       string `json:"com"`
+		Condition string `json:"condition"`
+		Status    string `json:"status"`
+		State     string `json:"state"`
+		Data      []struct {
+			Time     string `json:"time"`
+			AreaName string `json:"areaName,omitempty"`
+			Status   string `json:"status"`
+			AreaCode string `json:"areaCode,omitempty"`
+			Context  string `json:"context"`
+			Ftime    string `json:"ftime"`
+		} `json:"data"`
+	} `json:"lastResult"`
+	ComNew     string `json:"comNew"`
+	Billstatus string `json:"billstatus"`
+	AutoCheck  string `json:"autoCheck"`
+}
+
+// Poll 订阅邮件推送
+func Poll(key, company, number, tel, callbackurl string) (*StKuaidi100PollResp, error) {
+	retryCount := 0
+	reqBody := gin.H{
+		"company": company,
+		"number":  number,
+		"key":     key,
+		"parameters": gin.H{
+			"callbackurl": callbackurl,
+			"resultv2":    1,
+			"phone":       tel,
+		},
+	}
+	reqBytes, err := jsoniter.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+GotoHttpRetry:
+
+	_, body, errs := gorequest.New().
+		Post("https://poll.kuaidi100.com/poll").
+		Send("schema=json").
+		Send(fmt.Sprintf("param=%s", reqBytes)).
+		EndBytes()
+	if errs != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, errs[0]
+	}
+	var apiResp StKuaidi100PollResp
+	err = jsoniter.Unmarshal(body, &apiResp)
+	if err != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, fmt.Errorf("json err: %w %s", err, body)
+	}
+	if "200" != apiResp.ReturnCode && "501" != apiResp.ReturnCode {
+		return nil, fmt.Errorf("wx jscode err: %s %s", apiResp.ReturnCode, apiResp.Message)
+	}
+	return &apiResp, nil
+}
+
+// Query 获取快递信息
+func Query(customer, key, company, number, tel string) (*StKuaidi100GetResp, error) {
+	retryCount := 0
+	reqBody := gin.H{
+		"com":      company,
+		"num":      number,
+		"phone":    tel,
+		"resultv2": 2,
+	}
+	reqBytes, err := jsoniter.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+	rawStr := fmt.Sprintf("%s%s%s", reqBytes, key, customer)
+	data := []byte(rawStr)
+	r := md5.Sum(data)
+	signedString := hex.EncodeToString(r[:])
+	sign := strings.ToUpper(signedString)
+GotoHttpRetry:
+	_, body, errs := gorequest.New().
+		Post("https://poll.kuaidi100.com/poll/query.do").
+		Send(fmt.Sprintf("customer=%s", customer)).
+		Send(fmt.Sprintf("sign=%s", sign)).
+		Send(fmt.Sprintf("param=%s", reqBytes)).
+		EndBytes()
+	if errs != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, errs[0]
+	}
+	var apiResp StKuaidi100GetResp
+	err = jsoniter.Unmarshal(body, &apiResp)
+	if err != nil {
+		retryCount++
+		if retryCount < 3 {
+			goto GotoHttpRetry
+		}
+		return nil, fmt.Errorf("json err: %w %s", err, body)
+	}
+	return &apiResp, nil
+}
