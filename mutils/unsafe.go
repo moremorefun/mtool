7324bfdd6f91f971c41973bbcfc5af83bf3f4961
hao <unnamed.hao@gmail.com>
2021-02-05 10:17:27 +0800

    add string to bs

--- mutils/unsafe.go
+++ mutils/unsafe.go
@@ -1,6 +1,9 @@
 package mutils
 
-import "unsafe"
+import (
+	"reflect"
+	"unsafe"
+)
 
 // ByteSliceToString is used when you really want to convert a slice // of bytes to a string without incurring overhead. It is only safe
 // to use if you really know the byte slice is not going to change // in the lifetime of the string
@@ -8,4 +11,14 @@ func ByteSliceToString(bs []byte) string {
 	// This is copied from runtime. It relies on the string
 	// header being a prefix of the slice header!
 	return *(*string)(unsafe.Pointer(&bs))
+}
+
+// StringToBytes 字符串转bs
+func StringToBytes(s string) []byte {
+	l := len(s)
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{
+		Data: (*(*reflect.StringHeader)(unsafe.Pointer(&s))).Data,
+		Len:  l,
+		Cap:  l,
+	}))
 }
