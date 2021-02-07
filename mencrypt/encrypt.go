b8254de019fa05fd4e9180f1b9c5f0a65d2657dc
hao <unnamed.hao@gmail.com>
2021-02-07 15:39:41 +0800

    clear code

--- mencrypt/encrypt.go
+++ mencrypt/encrypt.go
@@ -12,10 +12,8 @@ import (
 // CBC
 // PKCS7Padding
 
-// AesEncrypt 加密
-func AesEncrypt(orig string, key string) (string, error) {
-	// 转成字节数组
-	origData := []byte(orig)
+// expandKey 扩充key长度
+func expandKey(key string) []byte {
 	k := []byte(key)
 	// 16, 24, 32
 	if len(k) < 16 {
@@ -27,6 +25,14 @@ func AesEncrypt(orig string, key string) (string, error) {
 	} else {
 		k = k[:32]
 	}
+	return k
+}
+
+// AesEncrypt 加密
+func AesEncrypt(orig string, key string) (string, error) {
+	// 转成字节数组
+	origData := []byte(orig)
+	k := expandKey(key)
 	// 分组秘钥
 	block, err := aes.NewCipher(k)
 	if err != nil {
@@ -39,29 +45,22 @@ func AesEncrypt(orig string, key string) (string, error) {
 	// 加密模式
 	blockMode := cipher.NewCBCEncrypter(block, k[:blockSize])
 	// 创建数组
-	cryted := make([]byte, len(origData))
+	encrypted := make([]byte, len(origData))
 	// 加密
-	blockMode.CryptBlocks(cryted, origData)
+	blockMode.CryptBlocks(encrypted, origData)
 
-	return base64.StdEncoding.EncodeToString(cryted), nil
+	return base64.StdEncoding.EncodeToString(encrypted), nil
 
 }
 
 // AesDecrypt aes解密
-func AesDecrypt(cryted string, key string) (string, error) {
+func AesDecrypt(encrypted string, key string) (string, error) {
 	// 转成字节数组
-	crytedByte, _ := base64.StdEncoding.DecodeString(cryted)
-	k := []byte(key)
-	// 16, 24, 32
-	if len(k) < 16 {
-		k = append(k, bytes.Repeat([]byte{0}, 16-len(k))...)
-	} else if len(k) < 24 {
-		k = append(k, bytes.Repeat([]byte{0}, 24-len(k))...)
-	} else if len(k) < 32 {
-		k = append(k, bytes.Repeat([]byte{0}, 32-len(k))...)
-	} else {
-		k = k[:32]
+	encryptedByte, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return "", err
 	}
+	k := expandKey(key)
 
 	// 分组秘钥
 	block, err := aes.NewCipher(k)
@@ -73,26 +72,26 @@ func AesDecrypt(cryted string, key string) (string, error) {
 	// 加密模式
 	blockMode := cipher.NewCBCDecrypter(block, k[:blockSize])
 	// 创建数组
-	orig := make([]byte, len(crytedByte))
+	orig := make([]byte, len(encryptedByte))
 	// 解密
-	blockMode.CryptBlocks(orig, crytedByte)
+	blockMode.CryptBlocks(orig, encryptedByte)
 	// 去补全码
 	orig = PKCS7UnPadding(orig)
 	return string(orig), nil
 }
 
 // PKCS7Padding 补码
-func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
-	padding := blocksize - len(ciphertext)%blocksize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padText...)
 }
 
 // PKCS7UnPadding 去码
 func PKCS7UnPadding(origData []byte) []byte {
 	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+	unPadding := int(origData[length-1])
+	return origData[:(length - unPadding)]
 }
 
 // DecryptAesEcb aes ecb 解密
