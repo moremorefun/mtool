2cc1d5fc2bef824bb46dff9b123ce960d0875580
hao <unnamed.hao@gmail.com>
2021-12-15 13:32:58 +0800

    add time func

--- mutils/utils.go
+++ mutils/utils.go
@@ -2,6 +2,7 @@ package mutils
 
 import (
 	"strings"
+	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -40,4 +41,36 @@ func IsInSlice(arr []interface{}, iv interface{}) bool {
 func GetUUIDStr() string {
 	u1 := uuid.NewV4()
 	return strings.Replace(u1.String(), "-", "", -1)
+}
+
+// GetLocalDayStart 获取当天开始时间
+func GetLocalDayStart() (*time.Time, error) {
+	cstSh := time.FixedZone("CST", 8*3600)
+	now := time.Now().In(cstSh)
+	nowStr := now.Format("2006-01-02")
+	localDayStart, err := time.ParseInLocation("2006-01-02", nowStr, cstSh)
+	if err != nil {
+		return nil, err
+	}
+	return &localDayStart, nil
+}
+
+// GetLocalWeekStart 获取周天开始时间
+func GetLocalWeekStart() (*time.Time, error) {
+	// 计算起始时间
+	cstSh := time.FixedZone("CST", 8*3600)
+	now := time.Now().In(cstSh)
+	nowStr := now.Format("2006-01-02")
+	localDayStart, err := time.ParseInLocation("2006-01-02", nowStr, cstSh)
+	if err != nil {
+		return nil, err
+	}
+	weekDay := localDayStart.Weekday()
+	if weekDay == 0 {
+		weekDay = 6
+	} else {
+		weekDay -= 1
+	}
+	dailyStart := localDayStart.AddDate(0, 0, -int(weekDay))
+	return &dailyStart, nil
 }
